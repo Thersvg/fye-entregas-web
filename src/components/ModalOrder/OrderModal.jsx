31012ea9c7b8a7a20882ca6be94681966d93af12
thersvg <rodrigohend@outlook.com>
2024-02-21 22:03:25 -0300

    updates

--- src/components/ModalOrder/OrderModal.jsx
+++ src/components/ModalOrder/OrderModal.jsx
@@ -39,6 +39,7 @@ export default function HandleModalOrder({ isOpen, onClose}){
           <label htmlFor="nameCliente">Cliente</label>
           <input
             type="text"
+            placeholder="Nome e sobrenome*"
             id="nameCliente"
             name="name_cliente"
             onChange={handleInputChange}
@@ -47,6 +48,7 @@ export default function HandleModalOrder({ isOpen, onClose}){
           <label htmlFor="endereco">Endereço</label>
           <input
             type="text"
+            placeholder="Rua/Número/Cidade*"
             id="endereco"
             name="endereco_cliente"
             onChange={handleInputChange}
@@ -55,6 +57,7 @@ export default function HandleModalOrder({ isOpen, onClose}){
           <label htmlFor="descricaoPedido">Descrição</label>
           <input
             type="text"
+            placeholder="Qual o pedido*"
             id="descricaoPedido"
             name="descricao_pedido"
             onChange={handleInputChange}
@@ -63,6 +66,7 @@ export default function HandleModalOrder({ isOpen, onClose}){
           <label htmlFor="telefoneCliente">Telefone</label>
           <input
             type="text"
+            placeholder="Whatsapp*"
             id="telefoneCliente"
             name="telefone_cliente"
             onChange={handleInputChange}
@@ -71,6 +75,7 @@ export default function HandleModalOrder({ isOpen, onClose}){
           <label htmlFor="valorPedido">Valor pedido</label>
           <input
             type="text"
+            placeholder="R$*"
             id="valorPedido"
             name="valor_pedido"
             onChange={handleInputChange}
@@ -79,6 +84,7 @@ export default function HandleModalOrder({ isOpen, onClose}){
           <label htmlFor="formaPagamento">Forma de pagamento</label>
           <input
             type="text"
+            placeholder="Pix/Dinheiro/Cartão de crédido*"
             id="formaPagamento"
             name="forma_pagamento"
             onChange={handleInputChange}
