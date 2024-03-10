d305fee29e238478baa19953d4cd746c97f53783
thersvg <rodrigohend@outlook.com>
2024-03-09 21:03:17 -0300

    updated

--- src/Pages/Welcome/Welcome.jsx
+++ src/Pages/Welcome/Welcome.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { Container, ContainerContent, EntryTop } from "./welcomeStyled";
 import Button from '@mui/material/Button';
+import imgbackground from '../../images/city.jpg'
 
 export function Welcome(){
 
@@ -8,6 +9,9 @@ export function Welcome(){
     return(
         <>
             <Container>
+                <div style={{width: '100%', height: '100%'}}>
+                    <img style={{width: 'auto', height: 'auto'}} src={''} alt="" />
+                </div>
             <EntryTop>
                 <div style={{width: '98%', display: 'flex' ,justifyContent: 'flex-end'}}>
                     <div>
@@ -16,6 +20,13 @@ export function Welcome(){
                 </div>
             </EntryTop>
                 <ContainerContent> 
+                    <div style={{gap: 15,borderRadius: '2%',backgroundColor:'#03bb85',width: '40%', height: '50%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+                        <h1 style={{fontSize: 60, color: '#fff'}}>Seja bem vindo!</h1>
+                        <p style={{color: '#fff'}}>Várias pessoas mudando de vida, mude a sua agora mesmo.</p>
+                        <div>
+                        <Button sx={{width: '100%',color: "#fff", fontWeight: 800, borderColor: '#fff',backgroundColor: '#transparent', ":hover": {border: 'none',backgroundColor:'#fff', color: '#03bb85'}}} onClick={() => navigate('/login')} variant="outlined">Cadastrar-se</Button> 
+                        </div>
+                    </div>
                 </ContainerContent>
             </Container>
         </>
--- src/Pages/Welcome/welcomeStyled.jsx
+++ src/Pages/Welcome/welcomeStyled.jsx
@@ -12,9 +12,18 @@ export const EntryTop = styled.div`
     align-items: center;
     justify-content: center;
     background: linear-gradient(90deg, rgba(3,187,133,1) 0%, rgba(4,179,186,1) 100%); 
+    position: absolute;
+    z-index: 1;
+    top: 0;
 `
 
 export const ContainerContent = styled.div`
     width: 100%;
     height: 90%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    z-index: 1;
+    top: 10%;
 `
